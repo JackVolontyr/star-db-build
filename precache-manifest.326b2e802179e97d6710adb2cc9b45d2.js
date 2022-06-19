57b3a82383dce6cb66f8852b0720e4695369fc80
Yevhen Volontyr <soapforwork@gmail.com>
2022-06-19 17:30:08 +0300

    init

---
+++ precache-manifest.326b2e802179e97d6710adb2cc9b45d2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05285881d851d3c12ba231cb039a4810",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e6b5181a324be798b19a",
+    "url": "/static/css/main.b3242da4.chunk.css"
+  },
+  {
+    "revision": "0265dad4b42a1e7186bf",
+    "url": "/static/js/2.a35e645f.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.a35e645f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6b5181a324be798b19a",
+    "url": "/static/js/main.e85e420c.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.e85e420c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2ca2d8e9eceb80b7104",
+    "url": "/static/js/runtime-main.2d85e0d0.js"
+  }
+]);
---
+++ precache-manifest.cc4e432a5cb4d2dcb1b1ae703f373db7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d470eb24178f1084bcc5ab3d88bf0905",
+    "url": "/star-db-build/index.html"
+  },
+  {
+    "revision": "e6b5181a324be798b19a",
+    "url": "/star-db-build/static/css/main.b3242da4.chunk.css"
+  },
+  {
+    "revision": "0265dad4b42a1e7186bf",
+    "url": "/star-db-build/static/js/2.a35e645f.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/star-db-build/static/js/2.a35e645f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6b5181a324be798b19a",
+    "url": "/star-db-build/static/js/main.e85e420c.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/star-db-build/static/js/main.e85e420c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de4dcb6e3b4a38441b10",
+    "url": "/star-db-build/static/js/runtime-main.756f5025.js"
+  }
+]);
